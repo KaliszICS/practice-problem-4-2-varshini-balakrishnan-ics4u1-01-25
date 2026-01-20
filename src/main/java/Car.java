85b4121c4a3abc9071babf2b6d3fd76a2bf3e3f5
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-20 15:26:37 +0000

    finished

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -15,4 +15,56 @@ class Car {
      * @param year - manufacured year of car
      * @param price - price of car 
      */
+
+    public Car(String make, String model, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+    /**
+     * @return - returns make of car
+     */
+    public String getMake(){
+        return make;
+    }
+    /**
+     * @return - returns model of car
+     */
+    public String getModel(){
+        return model;
+    }
+    /**
+     * @return - returns manufacture year of car
+     */
+    public int getYear(){
+        return year;
+    }
+    /**
+     * @return - returns price of car
+     */
+    public double getPrice(){
+        return price;
+    }
+    /**
+     * Updates make of car
+     * @param make - new make of car
+     */
+    public Void setMake(String make) {
+        this.make = make;
+    }
+    /**
+     * Updates model of car
+     * @param model - new model of car
+     */
+    public Void setModel(String model) {
+        this.model = model;
+    }
+    /**
+     * Updates manufacture year of car
+     * @param year - new  manufacture year of car
+     */
+    public Void setYear(int year) {
+        this.year = year;
+    }
 }
